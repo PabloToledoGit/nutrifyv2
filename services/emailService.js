700dc42ddd6a43a482ad310b4972cb0c899f2588
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2025-07-14 16:09:05 -0300

    atualizando valores

--- services/emailService.js
+++ services/emailService.js
@@ -16,8 +16,8 @@ export const enviarEmailComPDF = async (emailDestinatario, tituloReceita, pdfBuf
       port: 465,
       secure: true,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'nutrify@nutrifyservice.com',
+        pass: 'Pa199735@', // ⚠️ Só para teste
       },
       tls: {
         rejectUnauthorized: false
